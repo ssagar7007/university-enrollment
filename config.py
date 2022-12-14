0856e1b53de5987af5c8d76e013859d28c8caaac
ssagar7007 <92044580+ssagar7007@users.noreply.github.com>
2022-12-14 15:12:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-DB_URI = "mongodb+srv://sagar:sagar123@cluster0.rd1gxhm.mongodb.net/?retryWrites=true&w=majority"
+DB_URI = "mongodb+srv://<username>:<password>@cluster0.rd1gxhm.mongodb.net/?retryWrites=true&w=majority"
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or b'\x16\xbfI\xc9\x0f\xd2\xb1\x1f\xf2n\xb8`\x1frdw'
     
